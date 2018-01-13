7154dbb3c1a9ac1bfd94596ec0fc106c70e3d1b3
darklight <crossarium@gmail.com>
2018-01-13 17:28:53 +0200

    first commit

---
+++ 0.1/main.py
@@ -0,0 +1,12 @@
+import random
+
+count = 0
+question_number = int(input("How muany questions do you want to have?\n"))
+
+while count < question_number
+    number_1 = random.randint(1,100)
+    number_2 = random.randint(1,100)
+    right_answer = number_1 + number_2
+    input(number_1, "+",number_2, "=")
+
+
