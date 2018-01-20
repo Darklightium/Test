6b5076eac8da669712ed8f8913d81bd99241862c
darklight <crossarium@gmail.com>
2018-01-20 14:03:35 +0200

    first commit

--- 0.1/main.py
+++ 0.1/main.py
@@ -1,19 +1,19 @@
 import random
 
 count = 0
-answer = 0
+
 answer_count = 0
 question_number = int(input("How muany questions do you want to have?\n"))
 
 while count < question_number:
-    number_1 = random.randint(1, 100)
-    number_2 = random.randint(1, 100)
-    print(number_1, "+", number_2, "=")
+    number_1 = random.randint(1, 10)
+    number_2 = random.randint(1, 10)
+    print(number_1, "+", number_2)
     right_answer = number_1 + number_2
-    input(answer)
+    answer = int(input())
     if answer == right_answer:
-        answer_count = + 1
-    count = + 1
+        answer_count = answer_count + 1
+    count = count + 1
 print("Right answers are", answer_count, "of",question_number)
 
 
