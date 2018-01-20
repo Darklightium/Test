83fa6c5f2bbd82f478e1be6d3aab9a018e4dec90
darklight <crossarium@gmail.com>
2018-01-20 14:10:37 +0200

    first commit

--- 0.1/main.py
+++ 0.1/main.py
@@ -1,8 +1,21 @@
-def meter(road, feat, price):
-    return print(((road/100)*feat)*price)
+import random
+
+count = 0
+
+answer_count = 0
+question_number = int(input("How muany questions do you want to have?\n"))
+
+while count < question_number:
+    number_1 = random.randint(1, 10)
+    number_2 = random.randint(1, 10)
+    print(number_1, "+", number_2)
+    right_answer = number_1 + number_2
+    answer = int(input())
+    if answer == right_answer:
+        answer_count = answer_count + 1
+    count = count + 1
+print("Right answers are", answer_count, "of",question_number)
 
 
-road_u = float(input("Enter the lenth of way"))
-feat_u = float(input("Enter your car`s fuel"))
-price_u = float(input("Enter the price of fuel"))
-meter(road_u, feat_u, price_u)
+
+
